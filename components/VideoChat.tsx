f3c2cea4b90c3a94542df076c5df9c3c0e13ccef
localscriptx <86549883+missyoubutdontcomeback@users.noreply.github.com>
2026-09-11 22:39:18 +0700

    Update VideoChat.tsx

--- components/VideoChat.tsx
+++ components/VideoChat.tsx
@@ -308,7 +308,17 @@ export default function VideoChat({ userConfig, onStop }: VideoChatProps) {
         const configuration = {
             iceServers: [
                 { urls: 'stun:stun.l.google.com:19302' },
-                { urls: 'stun:stun1.l.google.com:19302' }
+                { urls: 'stun:stun1.l.google.com:19302' },
+                {
+                    urls: 'turn:openrelay.metered.ca:80',
+                    username: 'openrelayproject',
+                    credential: 'openrelayproject'
+                },
+                {
+                    urls: 'turn:openrelay.metered.ca:443',
+                    username: 'openrelayproject',
+                    credential: 'openrelayproject'
+                }
             ]
         };
 
